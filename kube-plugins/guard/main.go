3fd241f8314f684fe8e6d96d0664d16822284b60
techlead <rwingate@imaginestudio.io>
2023-06-05 08:00:33 -0700

    Create main.go

---
+++ kube-plugins/guard/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	logs "github.com/appscode/go/log/golog"
+	"github.com/appscode/guard/commands"
+)
+
+func main() {
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if err := commands.NewRootCmd().Execute(); err != nil {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
