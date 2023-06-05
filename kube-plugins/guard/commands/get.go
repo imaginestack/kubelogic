192d510f8ef221d38b8e086a658caa208a0fbebb
techlead <rwingate@imaginestudio.io>
2023-06-05 08:01:22 -0700

    Create get.go

---
+++ kube-plugins/guard/commands/get.go
@@ -0,0 +1,17 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func NewCmdGet() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:               "get",
+		Short:             `Get PKI`,
+		DisableAutoGenTag: true,
+	}
+	cmd.AddCommand(NewCmdGetWebhookConfig())
+	cmd.AddCommand(NewCmdGetToken())
+	cmd.AddCommand(NewCmdInstaller())
+	return cmd
+}
